027247327b0bb529d4e194d7c1cb25496d9ce02a
Nicksterr007 <96205586+Nicksterr007@users.noreply.github.com>
2022-03-18 22:32:53 -0400

    flashcard updated

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -18,14 +18,27 @@ class ViewController: UIViewController {
     @IBAction func didTaponFlashcard(_ sender: Any) {
         
         
-        if frontLabel.isHidden == true {
+       if frontLabel.isHidden == true {
             frontLabel.isHidden = false
-            
+
         }
         else {
             frontLabel.isHidden = true
         }
-    }
 
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+    }
 }
 
